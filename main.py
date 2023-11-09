feeec532230e65cfd36e249cba4d45ec75dde122
Niall <gallopn@ng-Ubuntu22.04.3>
2023-11-09 11:49:06 +0000

    refactored api call as an object worked into main.py. Pending cli incorporation.

--- main.py
+++ main.py
@@ -2,9 +2,42 @@
 Purpose: For testing objects
 Date: 07/11/2023
 ############################################################################'''
+import argparse
+import src.panelapp_requests as pan
+
+parser = argparse.ArgumentParser()
+
+# Create flags for use
+parser.add_argument("-r", "--r_number", dest = "r_number", help="e.g. -r R134")
+
+# assigning arguments and their associated flags to a variable
+args = parser.parse_args()
+
+# Take argument parsed as variable
+myR_Number = args.r_number
 
 def main():
-    return
+    # Create an instance of MyRequests
+    my_requests = pan.MyRequests(myR_Number)
+
+    # Make the API request
+    response = my_requests.request_data()
+
+    # Method of printing JSON structures in a more legible format
+    #json_str = json.dumps(response.json(), indent=4)
+    #print(json_str)
+
+    gene_list = []
+
+    for gene_number in range(len(response.json()["genes"])):
+        gene_symbol = response.json()["genes"][gene_number]["gene_data"]["gene_symbol"]
+        gene_list.append(gene_symbol)
+
+    # Print results to terminal screen
+    print("\n")
+    print("Clinical Indication:", response.json()["name"])
+    print(" ".join(["Genes included in the", myR_Number, "panel:", " ".join(gene_list)]))
+    print("\n")
 
 if __name__ == "__main__":
     main()
--- src/panelapp_requests.py
+++ src/panelapp_requests.py
@@ -1,23 +1,9 @@
 import requests
-import argparse
-import json
-
-
-parser = argparse.ArgumentParser()
-
-# Create flags for use
-parser.add_argument("-r", "--r_number", dest = "r_number", help="e.g. -r R134")
-
-# assigning arguments and their associated flags to a variable
-args = parser.parse_args()
-
-# Take argument parsed as variable
-myR_Number = args.r_number
 
 # Create class for requests
 class MyRequests:
 
-    def __init__(self):
+    def __init__(self, myR_Number):
         self.base_url = "https://panelapp.genomicsengland.co.uk/api/v1"
         # Update the URL to include the correct version and endpoint
         self.url = "".join(["/panels/", myR_Number]) #R334
@@ -25,27 +11,3 @@ class MyRequests:
     # Method that makes the call to the API using the get method
     def request_data(self):
         return requests.get(self.base_url + self.url)
-
-# Create an instance of MyRequests
-my_requests = MyRequests()
-
-# Make the API request
-response = my_requests.request_data()
-
-# Method of printing JSON structures in a more legible format
-#json_str = json.dumps(response.json(), indent=4)
-#print(json_str)
-
-gene_list = []
-
-for gene_number in range(len(response.json()["genes"])):
-    gene_symbol = response.json()["genes"][gene_number]["gene_data"]["gene_symbol"]
-    gene_list.append(gene_symbol)
-
-# Print results to terminal screen
-print("\n")
-print("Clinical Indication:", response.json()["name"])
-print(" ".join(["Genes included in the", myR_Number, "panel:", " ".join(gene_list)]))
-print("\n")
-
-
