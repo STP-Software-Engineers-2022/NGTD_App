18c4016a10df7eb6f345201881f878aa2dc6982d
Niall Gallop <62139335+NGallop@users.noreply.github.com>
2023-11-10 13:27:42 +0000

    functioning test for main, using monkeypatch to mock sys.argv

--- test/main_test.py
+++ test/main_test.py
@@ -3,12 +3,9 @@ Test executable python file
 ############################################################################'''
 
 import pytest
-'''
-import importlib as imp
-
-target = imp.load_source('main', 'main.py')
-'''
+import sys
 import main as target
 
-def test_main():
+def test_main(monkeypatch):
+    monkeypatch.setattr('sys.argv', ['pytest', '-r', 'R134'])
     assert target.main() == True
