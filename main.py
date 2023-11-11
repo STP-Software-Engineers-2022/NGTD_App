3c012d6348637bf1fab23989456032ed1a844cdd
DolapoA <dolaajayi@hotmail.com>
2023-11-11 00:57:32 +0000

    New bed flag for generating bed file and prepping data for database storage

--- main.py
+++ main.py
@@ -4,23 +4,24 @@ Date: 07/11/2023
 ############################################################################'''
 import sys
 import src.cli as cli
-import src.panelapp_requests as pan
+import src.panelapp_requests as panel
 
 def main():
 
     # initialise cli object
     cli_obj = cli.cli_obj(sys.argv[1:])
     r_code = cli_obj.args.r_number
-    store = cli_obj.args.store
+    bed = cli_obj.args.bed
 
     # Create an instance of MyRequests
-    my_requests = pan.MyRequests(r_code)
+    my_requests = panel.MyRequests(r_code)
 
     # Make the API request
     response = my_requests.request_data()
     my_requests.print_info(response, r_code)
 
-    my_requests.database_postage(response, r_code)
+    # Prepare data for database deposition
+    my_requests.database_postage(response, r_code, bed)
 
 if __name__ == "__main__":
     main()
--- src/panelapp_requests.py
+++ src/panelapp_requests.py
@@ -1,19 +1,26 @@
+'''####################################################################
+Purpose: For making requests to panelapp api, packaging data for  
+database deposition
+Date: 07/11/2023
+####################################################################'''
 import requests
 
 # Create class for requests
 class MyRequests:
 
-    def __init__(self, myR_Number):
+    def __init__(self, r_code):
         self.base_url = "https://panelapp.genomicsengland.co.uk/api/v1"
         # Update the URL to include the correct version and endpoint
-        self.url = "".join(["/panels/", myR_Number]) #R334
+        self.url = "".join(["/panels/", r_code])
 
     # Method that makes the call to the API using the get method
     def request_data(self):
         return requests.get(self.base_url + self.url)
     
+    # Method that prints key data to the terminal
     def print_info(self, response, r_code):
-        gene_list = [gene["gene_data"]["gene_symbol"] for gene in response.json()["genes"]]
+        g_list = [g["gene_data"]["gene_symbol"] \
+            for g in response.json()["genes"]]
 
         # Get GMS signed off status, checking whether a record exists or not
         try:
@@ -27,24 +34,40 @@ class MyRequests:
         else:
             print(" ".join(["\nThis panel is", signoff]))
         print("\nClinical Indication:", response.json()["name"])
-        print(" ".join(["\nGenes included in the", r_code, "panel:", " ".join(gene_list), "\n"]))
+        print(" ".join(["\nGenes included in the", r_code, "panel:", \
+            " ".join(g_list), "\n"]))
+    
+    # Method that packages data in dictionary ready for database & checks if user is okay with non-GMS signed panels
+    def database_postage(self, response, r_code, bed):
+        # bed is a boolean variable for whether the user wants a bed or not 
+        if bed:
+            # Get gene symbols and HGNC IDs in lists for dictionary    
+            g_list = [g["gene_data"]["gene_symbol"] \
+            for g in response.json()["genes"]]
 
-    def database_postage(self, response, r_code):
-        # Get gene symbols and HGNC IDs in lists for dictionary    
-        gene_list = [gene["gene_data"]["gene_symbol"] for gene in response.json()["genes"]]
-        hgnc_id_list = [gene["gene_data"]["hgnc_id"] for gene in response.json()["genes"]]
-        r_code = r_code # Reminder
+            h_list = [g["gene_data"]["hgnc_id"] \
+                for g in response.json()["genes"]]
 
-        # Get GMS signed off status, checking whether a record exists or not
-        try:
-            signoff = response.json()["types"][2]["name"]
-        except:
-            signoff = "not GMS signed-off"
+            # Get GMS signed off status, checking whether a record exists or not
+            try:
+                signoff = response.json()["types"][2]["name"]
+            except:
+                signoff = "not GMS signed-off"
 
-        # Create dictionary for R number
-        r_dict = {}
+            if signoff == "not GMS signed-off":
+                # Ask user for input
+                user_input = input("Do you want to continue with the analysis?\
+                    (yes/no): ").lower()
 
-        r_dict = {'r_number': r_code, 'signoff_status': signoff, 'genes': gene_list, 'hgnc_id_list': hgnc_id_list}
-        print(r_dict)
-        return r_dict
+                if user_input == "yes":
+                    # Initiate dictionary for R number
+                    r_dict = {}
+
+                    r_dict = {"r_number": r_code, "signoff_status": signoff, \
+                        "genes": g_list, "hgnc_id_list": h_list}
+                    print(r_dict)
+                    return r_dict
+                else:
+                    print(" ".join(["\nAnalysis ended due to", r_code, "not being GMS signed off."]))
+                    
         
