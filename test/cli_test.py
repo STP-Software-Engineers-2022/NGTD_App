66707a0fc40a411bb04aea995b5339bbc613dfbd
Niall Gallop <62139335+NGallop@users.noreply.github.com>
2024-01-17 14:38:19 +0000

    first couple of tests for create_beds. Noticed issue with code - output should be a changable attribute of the object. Will need to adjust after merge to finish testing.

--- test/cli_test.py
+++ test/cli_test.py
@@ -1,10 +1,14 @@
-"""###########################################################################
+"""
 Test Command Line Interface Object
-###########################################################################"""
+Author: Niall Gallop
+"""
 
 import pytest
 import src.command_line_interface as cli
 
+# Broad, basic functional test of CommandLineInterface
 def test_cli():
     parser = cli.CommandLineInterface(["-g", "-r", "R134"])
     assert parser.args.r_number == "R134"
+
+# Add error handling tests
